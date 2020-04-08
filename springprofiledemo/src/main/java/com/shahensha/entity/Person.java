1a7b57ea39738b4b32dbbc9e7c33660f9f592288
shahensha12 <skshahensha12@gmail.com>
2020-04-08 15:39:48 +0530

    update person class

--- springprofiledemo/src/main/java/com/shahensha/entity/Person.java
+++ springprofiledemo/src/main/java/com/shahensha/entity/Person.java
@@ -4,10 +4,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 //@Configuration
-//@Setter
-//@Getter
+//@Data
 //@AllArgsConstructor
+//@NoArgsConstructor
+//@EqualsAndHashCode
 //@ToString
+
 @Component
 @ConfigurationProperties(prefix="person")
 public class Person {
@@ -46,5 +48,36 @@ public class Person {
 	@Override
 	public String toString() {
 		return "Person [pid=" + pid + ", pname=" + pname + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((pid == null) ? 0 : pid.hashCode());
+		result = prime * result + ((pname == null) ? 0 : pname.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (pid == null) {
+			if (other.pid != null)
+				return false;
+		} else if (!pid.equals(other.pid))
+			return false;
+		if (pname == null) {
+			if (other.pname != null)
+				return false;
+		} else if (!pname.equals(other.pname))
+			return false;
+		return true;
 	}
 }
